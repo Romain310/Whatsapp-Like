6cf0d5252ed1fe8ca511c48d9608024efab9d2e1
Adri031 <Adri031@free.fr>
2024-07-12 13:42:08 +0200

    DATABASE_URL change

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,24 @@
+<?php
+
+// src/Controller/AdminController.php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+
+        return $this->render('admin.html.twig', [
+            'users' => $users,
+        ]);
+    }
+}
+
