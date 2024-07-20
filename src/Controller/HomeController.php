d3503dd2b0ff02ae40bb8d473050fe94af0c7131
Romain310 <romain.cazals@limayrac.fr>
2024-07-20 13:37:13 +0200

    Séparation lien commission-temporaire et commission-clos. Changement affichage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -69,4 +69,65 @@ class HomeController extends AbstractController
             return $this->redirectToRoute('home');
         }
     }
+
+    #[Route('/commission-temporaire/{idCommissionTemporaire}', name: 'messageCommissionTemporaireNonClos')]
+    public function messageCommissionTemporaireNonClos(EntityManagerInterface $entityManager, string $idCommissionTemporaire): Response
+    {
+        $dateActuelle = date('Y-m-d');
+
+        //Recupère la liste des commissions de la BDD
+        $commissions = $entityManager->getRepository(Commission::class)->findAll();
+        $commissionsTemporaireNonClos = $entityManager->getRepository(CommissionTemporaire::class)->findNonClos($dateActuelle);
+        $commissionsTemporaireClos = $entityManager->getRepository(CommissionTemporaire::class)->findClos($dateActuelle);
+
+        //Recupère le commission en fonction de son libelle
+        $commissionTemporaireNonClos = $entityManager->getRepository(CommissionTemporaire::class)->find($idCommissionTemporaire);
+
+        //Vérifie si la commission n'est pas cloturé
+        if ($commissionTemporaireNonClos && in_array($commissionTemporaireNonClos, $commissionsTemporaireNonClos)) {
+            //Initialise les paramètres envoyé à la vue
+            $params = [
+                "messages" => $commissionTemporaireNonClos->getMessages(),
+                "commissions" => $commissions,
+                "commissionsTemporaireNonClos" => $commissionsTemporaireNonClos,
+                "commissionsTemporaireClos" => $commissionsTemporaireClos,
+                "dateActuelle" => $dateActuelle
+            ];
+            //Affiche la vue
+            return $this->render('index.html.twig', $params);
+        } else {
+            //Si non trouvé renvoi sur la page home
+            return $this->redirectToRoute('home');
+        }
+    }
+    #[Route('/commission-clos/{idCommissionTemporaire}', name: 'messageCommissionTemporaireClos')]
+    public function messageCommissionTemporaireClos(EntityManagerInterface $entityManager, string $idCommissionTemporaire): Response
+    {
+        $dateActuelle = date('Y-m-d');
+
+        //Recupère la liste des commissions de la BDD
+        $commissions = $entityManager->getRepository(Commission::class)->findAll();
+        $commissionsTemporaireNonClos = $entityManager->getRepository(CommissionTemporaire::class)->findNonClos($dateActuelle);
+        $commissionsTemporaireClos = $entityManager->getRepository(CommissionTemporaire::class)->findClos($dateActuelle);
+
+        //Recupère le commission en fonction de son libelle
+        $commissionTemporaireClos = $entityManager->getRepository(CommissionTemporaire::class)->find($idCommissionTemporaire);
+
+        //Vérifie si la commission est cloturé
+        if ($commissionTemporaireClos && in_array($commissionTemporaireClos, $commissionsTemporaireClos)) {
+            //Initialise les paramètres envoyé à la vue
+            $params = [
+                "messages" => $commissionTemporaireClos->getMessages(),
+                "commissions" => $commissions,
+                "commissionsTemporaireNonClos" => $commissionsTemporaireNonClos,
+                "commissionsTemporaireClos" => $commissionsTemporaireClos,
+                "dateActuelle" => $dateActuelle
+            ];
+            //Affiche la vue
+            return $this->render('index.html.twig', $params);
+        } else {
+            //Si non trouvé renvoi sur la page home
+            return $this->redirectToRoute('home');
+        }
+    }
 }
