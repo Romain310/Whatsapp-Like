5e10dfd10e01d1807797acbb7b3d53729532429f
Val-Nost <valentin.smn81@gmail.com>
2024-07-20 19:29:25 +0200

    Ajout_EnvoieMessage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,8 +4,14 @@ namespace App\Controller;
 
 use App\Entity\Commission;
 use App\Entity\CommissionTemporaire;
+use App\Entity\Message;
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\OptimisticLockException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,16 +30,14 @@ class HomeController extends AbstractController
         //Recupère objet commission général (ID 1)
         $commissionGeneral = $entityManager->getRepository(Commission::class)->find(1);
 
-        //Récupère les messages de commission général
-        $messages = $commissionGeneral->getMessages();
-
         //Initialise les paramètres envoyé à la vue
         $params = [
             "messages" => $commissionGeneral->getMessages(),
             "commissions" => $commissions,
             "commissionsTemporaireNonClos" => $commissionsTemporaireNonClos,
             "commissionsTemporaireClos" => $commissionsTemporaireClos,
-            "dateActuelle" => $dateActuelle
+            "dateActuelle" => $dateActuelle,
+            "commissionSelected" => $commissionGeneral
         ];
 
         //Affiche la vue
@@ -41,7 +45,7 @@ class HomeController extends AbstractController
     }
 
     #[Route('/commission/{libelleCommission}', name: 'messageCommission')]
-    public function messageCommission(EntityManagerInterface $entityManager, string $libelleCommission): Response
+    public function messageCommission(Request $request, EntityManagerInterface $entityManager, string $libelleCommission): Response
     {
         $dateActuelle = date('Y-m-d');
 
@@ -60,7 +64,8 @@ class HomeController extends AbstractController
                 "commissions" => $commissions,
                 "commissionsTemporaireNonClos" => $commissionsTemporaireNonClos,
                 "commissionsTemporaireClos" => $commissionsTemporaireClos,
-                "dateActuelle" => $dateActuelle
+                "dateActuelle" => $dateActuelle,
+                "commissionSelected" => $commission
             ];
             //Affiche la vue
             return $this->render('index.html.twig', $params);
@@ -81,17 +86,18 @@ class HomeController extends AbstractController
         $commissionsTemporaireClos = $entityManager->getRepository(CommissionTemporaire::class)->findClos($dateActuelle);
 
         //Recupère le commission en fonction de son libelle
-        $commissionTemporaireNonClos = $entityManager->getRepository(CommissionTemporaire::class)->find($idCommissionTemporaire);
+        $commissionSelected = $entityManager->getRepository(CommissionTemporaire::class)->find($idCommissionTemporaire);
 
         //Vérifie si la commission n'est pas cloturé
-        if ($commissionTemporaireNonClos && in_array($commissionTemporaireNonClos, $commissionsTemporaireNonClos)) {
+        if ($commissionSelected != null) {
             //Initialise les paramètres envoyé à la vue
             $params = [
-                "messages" => $commissionTemporaireNonClos->getMessages(),
+                "messages" => $commissionSelected->getMessages(),
                 "commissions" => $commissions,
                 "commissionsTemporaireNonClos" => $commissionsTemporaireNonClos,
                 "commissionsTemporaireClos" => $commissionsTemporaireClos,
-                "dateActuelle" => $dateActuelle
+                "dateActuelle" => $dateActuelle,
+                "commissionTemporaireSelected" => $commissionSelected
             ];
             //Affiche la vue
             return $this->render('index.html.twig', $params);
@@ -111,23 +117,56 @@ class HomeController extends AbstractController
         $commissionsTemporaireClos = $entityManager->getRepository(CommissionTemporaire::class)->findClos($dateActuelle);
 
         //Recupère le commission en fonction de son libelle
-        $commissionTemporaireClos = $entityManager->getRepository(CommissionTemporaire::class)->find($idCommissionTemporaire);
+        $commissionSelected = $entityManager->getRepository(CommissionTemporaire::class)->find($idCommissionTemporaire);
 
         //Vérifie si la commission est cloturé
-        if ($commissionTemporaireClos && in_array($commissionTemporaireClos, $commissionsTemporaireClos)) {
+        if ($commissionSelected != null) {
             //Initialise les paramètres envoyé à la vue
             $params = [
-                "messages" => $commissionTemporaireClos->getMessages(),
+                "messages" => $commissionSelected->getMessages(),
                 "commissions" => $commissions,
                 "commissionsTemporaireNonClos" => $commissionsTemporaireNonClos,
                 "commissionsTemporaireClos" => $commissionsTemporaireClos,
-                "dateActuelle" => $dateActuelle
+                "dateActuelle" => $dateActuelle,
+                "commissionTemporaireSelected" => $commissionSelected
             ];
             //Affiche la vue
             return $this->render('index.html.twig', $params);
         } else {
             //Si non trouvé renvoi sur la page home
             return $this->redirectToRoute('home');
+        }
+    }
+
+    #[Route('/sendMessage', name: 'sendMessages', methods: ['POST'])]
+    public function sendMessages(EntityManagerInterface $entityManager, Request $request): RedirectResponse
+    {
+        $user = $entityManager->getRepository(User::class)->find($request->get('user'));
+        $message = new Message();
+        $message->setContenu($request->get("message"));
+        $message->setDateEnvoi(new \DateTime("now"));
+        $message->setUser($user);
+
+        // Si la requête contient l'id de la commission
+        if ($request->get('commission') != null) {
+            $commission = $entityManager->getRepository(Commission::class)->find($request->get("commission"));
+            $message->addCommission($commission);
+        }
+
+        // Si la requête contient l'id de la commission temporaire
+        if ($request->get('commissionTemporaire') != null) {
+            $commissionTemporaire = $entityManager->getRepository(CommissionTemporaire::class)->find($request->get("commissionTemporaire"));
+            $message->addCommissionsTemporaire($commissionTemporaire);
+        }
+
+        $entityManager->persist($message);
+        $entityManager->flush();
+        if (!$message->getCommissions()->isEmpty()) {
+            return $this->redirect('/commission/'.$message->getCommissions()[0]->getLibelle());
+        } elseif (!$message->getCommissionsTemporaires()->isEmpty()) {
+            return $this->redirect('/commission-temporaire/'.$message->getCommissionsTemporaires()[0]->getId());
+        } else {
+            return $this->redirect('/');
         }
     }
 }
