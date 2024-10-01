4b8122412761195717a4266940993cf9eddaf0ca
Adri031 <Adri031@free.fr>
2024-10-01 20:00:11 +0200

    Page admin Alpha

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,13 +1,14 @@
 <?php
 
-// src/Controller/AdminController.php
-
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends AbstractController
 {
@@ -20,5 +21,18 @@ class AdminController extends AbstractController
             'users' => $users,
         ]);
     }
-}
 
+    #[Route('/admin/delete/{id}', name: 'admin_delete')]
+    public function delete(User $user, EntityManagerInterface $em, Request $request): Response
+    {
+        // Vérifie le token CSRF
+        if ($this->isCsrfTokenValid('delete-user-' . $user->getId(), $request->request->get('_token'))) {
+            $em->remove($user);
+            $em->flush();
+
+            $this->addFlash('success', 'Utilisateur supprimé avec succès.');
+        }
+
+        return $this->redirectToRoute('admin');
+    }
+}
