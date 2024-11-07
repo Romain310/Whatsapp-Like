ae03930923a9f67310aff535e06bbc55235dfac8
Val-Nost <valentin.smn81@gmail.com>
2024-11-07 14:48:00 +0100

    Rework_Notifications
    On simplifie pour éviter de rajouter une page à rallonge

--- src/Controller/NotificationsController.php
+++ src/Controller/NotificationsController.php
@@ -23,51 +23,6 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 class NotificationsController extends AbstractController
 {
-    #[Route('/gestionNotifications', name: 'gestionNotifications')]
-    public function gestionNotifications(NotificationCommissionRepository $notificationCommissionRepository,
-                          NotificationCommissionTemporaireRepository $notificationCommissionTemporaireRepository,
-                          UserInterface $user): Response
-    {
-        $notificationsCommissions = $notificationCommissionRepository->findByUser($user);
-        $notificationsCommissionsTemporaires = $notificationCommissionTemporaireRepository->findByUser($user);
-        return $this->render('notification.html.twig', [
-            'notificationsCommissions' => $notificationsCommissions,
-            'notificationsCommissionsTemporaires' => $notificationsCommissionsTemporaires,
-        ]);
-    }
-    #[Route('/updateNotifications', name: 'updateNotification', methods: ['POST'])]
-    public function updateNotifications(Request $request,
-                                        EntityManagerInterface $entityManager,
-                                        CommissionRepository $commissionRepository,
-                                        CommissionTemporaireRepository $commissionTemporaireRepository,
-                                        NotificationCommissionRepository $notificationCommissionRepository,
-                                        NotificationCommissionTemporaireRepository $notificationCommissionTemporaireRepository,
-                                        UserInterface $user)
-    {
-        $entierCommission = array();
-        $entierCommissionTemporaire = array();
-        $test = [];
-        foreach ($request->getPayload()->keys() as $key) {
-            if (preg_match('/commission\\d+/', $key)) {
-                preg_match('/\\d+/', $key, $idCommission);
-                $commission = $commissionRepository->find($idCommission[0]);
-                $notificationCommission = $notificationCommissionRepository->findByUserAndCommission($user, $commission);
-                $notificationCommission[0]->setActive($request->get($key));
-                $entityManager->persist($notificationCommission[0]);
-                $entityManager->flush();
-            }
-            if (preg_match('/commissionTemporaire\\d+/', $key)) {
-                preg_match('/\\d+/', $key, $idCommission);
-                $commissionTemporaire = $commissionTemporaireRepository->find($idCommission[0]);
-                $notificationCommissionTemporaire = $notificationCommissionTemporaireRepository->findByUserAndCommissionTemporaire($user, $commissionTemporaire);
-                $notificationCommissionTemporaire[0]->setActive($request->get($key));
-                $entityManager->persist($notificationCommissionTemporaire[0]);
-                $entityManager->flush();
-            }
-        }
-        return $this->redirectToRoute('gestionNotifications');
-    }
-
     #[Route('/gestionNotificationCommission', name: 'gestionNotificationCommission')]
     public function gestionNotificationCommission(Request $request, EntityManagerInterface $entityManager): Response
     {
